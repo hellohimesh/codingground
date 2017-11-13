df61b501108a466fb56934dd60fdb8d31bf0290e
Himesh Suthar <himesh5983@gmail.com>
2017-11-13 22:54:19 +0530

    Created by Coding Ground Client

---
+++ New Project-20171113/main.r
@@ -0,0 +1,9 @@
+# Get and print current working directory.
+print(getwd())
+setwd("/web/com")
+
+# Get and print current working directory.
+print(getwd())
+
+install.packages("xlsx")
+any(grepl("xlsx",installed.packages()))
